88d572cb0cd61ab1a09446543a0504d51547c4d2
Meryem Ben Jouda <miryambjzidi@gmail.com>
2022-09-19 18:41:40 +0200

    component myFavorites get and post requests fonctionnal

---
+++ src/components/MyArtists/MyArtists.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Artist from "../AllArtists/Artist";
+
+function MyArtists() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://women-of-hip-hop.herokuapp.com/artists/myArtists", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {
+        console.log("response.data", response.data);
+        setArtists(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <h3>All the artists you created</h3>
+      <ul>
+        {artists.map((artist) => {
+          return (
+            <div key={artist._id}>
+              <Artist
+                name={artist.name}
+                picture={artist.picture}
+                flagSong={artist.flagSong}
+                miniBio={artist.miniBio}
+              />
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default MyArtists;
