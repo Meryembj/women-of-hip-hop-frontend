01ea19fde6ce22255bee4b4c87cd9fd87b0521fa
Meryem Ben Jouda <miryambjzidi@gmail.com>
2022-09-20 14:58:21 +0200

    delete myArtist myAlbum and icons change

--- src/components/MyAlbums/MyAlbumCard.jsx
+++ src/components/MyAlbums/MyAlbumCard.jsx
@@ -1,6 +1,50 @@
 import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const MyAlbum = ({ name, picture, artist }) => {
+const API_URL = "https://women-of-hip-hop.herokuapp.com/albums";
+
+const MyAlbumCard = ({ name, picture, artist, album }) => {
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    console.log(album);
+    event.preventDefault();
+    axios
+      .delete(`${API_URL}/${album._id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+  /*   useEffect(() => {
+    fetchData();
+}, [data]); */
+  /* 
+  useEffect((id) => {
+    axios
+      .delete("https://women-of-hip-hop.herokuapp.com/artists/myArtists" + id, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {
+        navigate("/myAlbums");
+        setStatus(response.data);
+      })
+      .catch((err) => console.log(err));
+  }, []); */
+
   return (
     <div class="container">
       <div class="row">
@@ -11,12 +55,19 @@ const MyAlbum = ({ name, picture, artist }) => {
               <img class="card-img-top" src={picture} alt="Card cap"></img>
               <div class="card-header">{name}</div>
               <div class="card-body">
+                <a href="#" class="btn btn-success">
+                  Add to favorite
+                </a>
                 <a href="#" class="btn btn-primary">
                   Details
                 </a>
-                <a href="#" class="btn btn-danger">
+                <button
+                  href="#"
+                  class="btn btn-danger"
+                  onClick={(event) => handleSubmit(event)}
+                >
                   Delete
-                </a>
+                </button>
               </div>
             </div>
           </div>
@@ -26,4 +77,4 @@ const MyAlbum = ({ name, picture, artist }) => {
   );
 };
 
-export default MyAlbum;
+export default MyAlbumCard;
--- src/components/MyArtists/MyArtistCard.jsx
+++ src/components/MyArtists/MyArtistCard.jsx
@@ -1,30 +1,56 @@
 import React from "react";
 import Navbar from "../Navbar";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { FcLike, FcLikePlaceholder, FcFullTrash } from "react-icons/fc";
 
-const myArtist = ({ picture, name, miniBio, flagSong }) => {
+const API_URL = "https://women-of-hip-hop.herokuapp.com/artists";
+
+const MyArtistCard = ({ picture, name, miniBio, flagSong, artist }) => {
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const handleSubmit = (event) => {
+    console.log(artist);
+    event.preventDefault();
+    axios
+      .delete(`${API_URL}/${artist._id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      })
+      .then((response) => {})
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
   return (
-      <div>
-        <div class="container">
-          <div class="row">
-            <div class="col">
-              <div class="card" style={{ width: "18rem" }}>
-                <img class="card-img-top" src={picture} alt="profile"></img>
-                <div class="card-body">
-                  <h5 class="card-title">{name}</h5>
-                  <p class="card-text">{miniBio}</p>
-                  <p class="card-text">{flagSong}</p>
-                  <a href="#" class="btn btn-success">
-                    Add to favorite
-                  </a>
-                  <a href="#" class="btn btn-danger">
-                    Remove
-                  </a>
-                </div>
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card" style={{ width: "18rem" }}>
+              <img class="card-img-top" src={picture} alt="profile"></img>
+              <div class="card-body">
+                <h5 class="card-title">{name}</h5>
+                <p class="card-text">{miniBio}</p>
+                <p class="card-text">{flagSong}</p>
+                <FcLikePlaceholder>
+                  <FcLike></FcLike>
+                </FcLikePlaceholder>
+                <button href="#" class="btn btn-primary">
+                  Details
+                </button>
+                <FcFullTrash href="#" onClick={(event) => handleSubmit(event)}>
+                  Delete
+                </FcFullTrash>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
-export default myArtist;
+export default MyArtistCard;
